d04c3cb63a0394e298534d7d9ea88f55c42bc384
Disco4uf <Disco4uf@users.noreply.github.com>
2020-03-24 21:45:19 +0430

    Fix1

--- src/com/company/Classes/Constant.java
+++ src/com/company/Classes/Constant.java
@@ -5,6 +5,9 @@ public class Constant {
 	public  static String regexRegister="register (.*?) (.*?)$";
 	public  static String regexLogin="login (.*?) (.*?)$";
 	public  static String regexRemove="remove (.*?) (.*?)$";
+
+	//========================================================
+
 	public  static String errNotExistPlayer="no user exists with this username";
 	public  static String errInvalidUsername="username format is invalid";
 	public  static String errInvalidPass="password format is invalid";
