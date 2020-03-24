9e8d66b178f397e27e5864f817d613068fc48bb0
Disco4uf <Disco4uf@users.noreply.github.com>
2020-03-25 00:38:46 +0430

    Update ChessGame.java

--- src/com/company/Structs/ChessGame.java
+++ src/com/company/Structs/ChessGame.java
@@ -9,11 +9,13 @@ public class ChessGame {
     Player Player2;
 
     Player CurrentPlayer;
+    int Limit;
     int CurrentMoves=0;
-    public ChessGame(Player p1,Player p2){
+    public ChessGame(Player p1,Player p2,int limit){
         this.Player1=p1;
         this.Player2=p2;
         this.CurrentMoves=0;
+        this.Limit=Limit;
         TheGamePieces=new ChessPiece[9][9];
       //=========================================
     }
