7d6fb346d8a167ee40c868878dd198751ae132a0
Arash_ST <31709401+ArashST79@users.noreply.github.com>
2020-03-25 03:00:09 +0430

    tw

--- src/com/company/Classes/myFunc.java
+++ src/com/company/Classes/myFunc.java
@@ -6,61 +6,59 @@ import java.util.List;
 
 public class myFunc {
 
-
-    public static double Distance(int fx,int fy,int tx,int ty){
-        return ((fx-tx)^2)+((fy-ty)^2);
-    }
     public static Object SearchInList(List<Object> tList, Object search) {
-        for (Object v:tList) {
-            if(v==search)return v;
+        for (Object v : tList) {
+            if (v == search) return v;
         }
         return null;
     }
-    private static List<Player> playersOrderAlphabetically(List<Player> players){
-        for (int i=0;i<players.size();i++){
-            for(int j=0;j<players.size()-1-i;j++){
-                if(players.get(j).getPlayerName().compareTo(players.get(j+1).getPlayerName())>0){
+
+    private static List<Player> playersOrderAlphabetically(List<Player> players) {
+        for (int i = 0; i < players.size(); i++) {
+            for (int j = 0; j < players.size() - 1 - i; j++) {
+                if (players.get(j).getPlayerName().compareTo(players.get(j + 1).getPlayerName()) > 0) {
                     Player player = players.get(j);
-                    players.set(j,players.get(j+1));
-                    players.set(j+1,player);
+                    players.set(j, players.get(j + 1));
+                    players.set(j + 1, player);
                 }
             }
         }
         return players;
     }
-    public static void printPlayerNamesByOrder(List<Player> players,Boolean isScoarBoard){
-        if(!isScoarBoard) {
+
+    public static void printPlayerNamesByOrder(List<Player> players, Boolean isScoarBoard) {
+        if (!isScoarBoard) {
             List<Player> p = playersOrderAlphabetically(players);
-        }
-        else{
+        } else {
             List<Player> p = playersOrderScoreboard(players);
         }
-        for(int i=0;i<players.size();i++){
+        for (int i = 0; i < players.size(); i++) {
             System.out.println(players.get(i).getPlayerName());
         }
     }
-    private static List<Player> swichPlayers(List<Player> players,int index1,int index2){
-        players = swichPlayers(players,j,j+1);
+
+    private static List<Player> swichPlayers(List<Player> players, int index1, int index2) {
+        Player player = players.get(index1);
+        players.set(index1,players.get(index2));
+        players.set(index2,player);
         return players;
     }
-    private static List<Player> playersOrderScoreboard(List<Player> players){
-        for (int i=0;i<players.size();i++){
-            for(int j=0;j<players.size()-1-i;j++){
+
+    private static List<Player> playersOrderScoreboard(List<Player> players) {
+        for (int i = 0; i < players.size(); i++) {
+            for (int j = 0; j < players.size() - 1 - i; j++) {
                 Player p1 = players.get(j);
-                Player p2 = players.get(j+1);
-                if(p1.getScore()<p2.getScore()){
-                    players = swichPlayers(players,j,j+1);
-                }
-                else if(p1.getScore()==p2.getScore()){
-                    if(p1.getWin()<p2.getWin()){
-                        players = swichPlayers(players,j,j+1);
-                    }
-                    else if(p1.getWin()==p2.getWin()) {
-                        if(p1.getDraw()<p2.getDraw()) {
+                Player p2 = players.get(j + 1);
+                if (p1.getScore() < p2.getScore()) {
+                    players = swichPlayers(players, j, j + 1);
+                } else if (p1.getScore() == p2.getScore()) {
+                    if (p1.getWin() < p2.getWin()) {
+                        players = swichPlayers(players, j, j + 1);
+                    } else if (p1.getWin() == p2.getWin()) {
+                        if (p1.getDraw() < p2.getDraw()) {
                             players = swichPlayers(players, j, j + 1);
-                        }
-                        else if(players.get(j).getPlayerName().compareTo(players.get(j+1).getPlayerName())>0)
-                            players = swichPlayers(players,j,j+1);
+                        } else if (players.get(j).getPlayerName().compareTo(players.get(j + 1).getPlayerName()) > 0)
+                            players = swichPlayers(players, j, j + 1);
                     }
                 }
 
--- src/com/company/Structs/ChessGame.java
+++ src/com/company/Structs/ChessGame.java
@@ -1,14 +1,10 @@
 package com.company.Structs;
 
 import com.company.Classes.Constant;
-import com.company.Classes.myFunc;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ChessGame {
-
-    List<ChessMove> GameMoves;
     ChessPiece[][] theGamePieces;
 
     Player player1;
@@ -17,6 +13,7 @@ public class ChessGame {
     Player currentPlayer;
     int limit;
     int currentMoves=0;
+<<<<<<< Updated upstream
     public ChessGame(Player p1,Player p2,int limit){
         this.player1=p1;
         this.player2=p2;
@@ -24,6 +21,14 @@ public class ChessGame {
         this.limit=limit;
         theGamePieces=new ChessPiece[9][9];
         GameMoves=new ArrayList<ChessMove>();
+=======
+    public ChessGame(Player p1,Player p2,int limit) {
+        this.player1 = p1;
+        this.player2 = p2;
+        this.currentMoves = 0;
+        this.limit = limit;
+        theGamePieces = new ChessPiece[9][9];
+>>>>>>> Stashed changes
         //=========================================
 
         if(p1.getPlayerColor()==p2.getPlayerColor()){
@@ -36,17 +41,16 @@ public class ChessGame {
         for (ChessPiece p:
         ChessPiece.CreatePieces(p1)) {
             theGamePieces[p.OriginalX][p.OriginalY]=p;
-            p.isFirstMove=true;
         }
         for (ChessPiece p:
                 ChessPiece.CreatePieces(p2)) {
             theGamePieces[p.OriginalX][p.OriginalY]=p;
-            p.isFirstMove=true;
         }
-        currentPlayer=player1;
         //=========================================
-        //PrintBoard(true);
+
+        PrintBoard(true);
     }
+<<<<<<< Updated upstream
 
     public boolean MakeMove(int fx,int fy,int tx,int ty){
         if(fx<1||fy<1||tx<1||ty<1
@@ -62,22 +66,26 @@ public class ChessGame {
         if(fromPiece==null){
             //invalid
             return false;
+=======
+    public static void startTheGame(String p1,String p2,int limit){
+        if(!p1.matches(Constant.regexAcceptableCharacters)){
+            System.out.println(Constant.errInvalidUsername);
+            return;
+>>>>>>> Stashed changes
         }
-
-        if(fromPiece.OwnerColor!=currentPlayer.getPlayerColor()){
-            //invalid  , NOT your turn
-            return false;
+        else if(limit<0){
+            System.out.println(Constant.errInvalidLimit);
+            return;
         }
-
-        if(toPiece!=null&&fromPiece.OwnerColor==toPiece.OwnerColor){
-            //invalid, same color
-            return false;
+        else if(p1.equals(p2)) {
+            System.out.println(Constant.errChooseAnotherPlayer);
+            return;
         }
-
-        if(!isValidMove(fromPiece,toPiece,tx,fy,tx,ty)){
-            //invalid , cant make that move
-            return false;
+        else if(Player.getPlayerByName(p2)==null) {
+            System.out.println(Constant.errNotExistPlayer);
+            return;
         }
+<<<<<<< Updated upstream
 
         //===================================
         //Make the move
@@ -89,61 +97,21 @@ public class ChessGame {
         theGamePieces[fx][fy]=null;
         fromPiece.isFirstMove=false;
         return true;
-    }
-    public boolean isValidMove(ChessPiece p,ChessPiece tp,int fx,int fy,int tx,int ty){
-
-        if(tp!=null&&p.OwnerColor==tp.OwnerColor)return false;
-
-        switch (p.name){
-            case "king":
-                return myFunc.Distance(fx,fy,tx,ty) <= 1; // if distance is lower than 1
-            case "pawn":
-                if(tp!=null){//Capture
-                    //check move
-                    if(p.OwnerColor== Constant.PlayerColor.White){
-
-                        //Valid pos in CAPTURE MODE
-
-                        if(ty!=fy-1)return false;
-
-                        if((tx==fx-1)
-                           ||(tx==fx+1&&tx>0))return true;
-
-                        return false;
-                    }
-
-                    return true;
-                }else if(p.isFirstMove){
-
-                }
-
-                break;
-            case "rook":
-                break;
-            case "knight":
-                break;
+=======
+        else {
+            ChessGame theGame = new ChessGame(Player.getPlayerByName(p1),Player.getPlayerByName(p2),limit);
+            Menu.setMenuSituation(Menu.situation.gameMenue);
         }
-        return true;
-    }
 
-    public ChessPiece GetPieceAtPos(int x,int y){
-        if(x>0&&y>0&&x<=8&&y<=8)return theGamePieces[x][y];
-        else return null;
+>>>>>>> Stashed changes
     }
-    public void PrintBoard(boolean fancy,boolean flip)
-    {
+    public void PrintBoard(boolean fancy){
         boolean IsFirst=true;
         for (int i = 1; i < theGamePieces.length; i++) {
 
             IsFirst=true;
             for (int j = 1; j < theGamePieces[i].length; j++) {
-                ChessPiece p;
-                if(flip){
-                    p=theGamePieces[9-i][j];
-                }else{
-                    p=theGamePieces[i][j];
-                }
-
+                ChessPiece p=theGamePieces[i][j];
 
                 if(IsFirst)IsFirst=false;
                 else System.out.print("|");
@@ -160,27 +128,6 @@ public class ChessGame {
             System.out.print("\n");
         }
     }
-
-    private static void startTheGame(String p1,String p2,int limit){
-        if(!p1.matches(Constant.regexAcceptableCharacters)){
-            System.out.println(Constant.errInvalidUsername);
-            return;
-        }
-        else if(limit<0){
-            System.out.println(Constant.errInvalidLimit);
-            return;
-        }
-        else if(p1.equals(p2)) {
-            System.out.println(Constant.errChooseAnotherPlayer);
-            return;
-        }
-        else if(Player.getPlayerByName(p2)==null) {
-            System.out.println(Constant.errNotExistPlayer);
-            return;
-        }
-
-    }
-
 
 
 }
