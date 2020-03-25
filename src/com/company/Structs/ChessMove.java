a8d592865bdf2e3e22f6b3b14d2a03442d29a025
Disco4uf <Disco4uf@users.noreply.github.com>
2020-03-25 12:32:26 +0430

    Update ChessMove.java

--- src/com/company/Structs/ChessMove.java
+++ src/com/company/Structs/ChessMove.java
@@ -12,22 +12,46 @@ public class ChessMove {
 
         this.fromPiece=fromPiece;
         this.toPiece=toPiece;
-        this.fromX=fromPiece.x;
-        this.fromY=fromPiece.y;
-        this.toX=toPiece.x;
-        this.toY=toPiece.y;
+
+        if(fromPiece!=null) {
+            this.fromX = fromPiece.x;
+            this.fromY = fromPiece.y;
+        }else{
+            this.fromX =-1;
+            this.fromY =-1;
+        }
+        if(toPiece!=null){
+            this.toX=toPiece.x;
+            this.toY=toPiece.y;
+        }else{
+            this.toX=-1;
+            this.toY=-1;
+        }
+
         this.theGamePiecesBeforeMove=theGamePiecesBeforeMove;
     }
 
     public ChessMove(ChessPiece[][] theGamePiecesBeforeMove,int fx,int fy,int tx,int ty){
+
+
 
         this.fromX=fx;
         this.fromY=fy;
         this.toX=tx;
         this.toY=ty;
 
-        fromPiece=theGamePiecesBeforeMove[fx][fy];
-        toPiece=theGamePiecesBeforeMove[toX][toY];
+        if(fx>=1&&fy>=1&&fx<=8&&fy<=8){
+            fromPiece=theGamePiecesBeforeMove[fx][fy];
+        }else{
+            fromPiece=null;
+        }
+
+        if(tx>=1&&ty>=1&&tx<=8&&ty<=8){
+            toPiece=theGamePiecesBeforeMove[tx][ty];
+        }else{
+            toPiece=null;
+        }
+
 
         this.theGamePiecesBeforeMove=theGamePiecesBeforeMove;
 
